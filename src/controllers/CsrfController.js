0c7f8f5b24f3729eb112f12e3f70bae137a9ec8b
Juan Luis Talavera Peláez <jcooldevelopment@gmail.com>
2025-08-07 00:41:09 +0200

    Enviar csrf token en el body de la respuesta

--- src/controllers/CsrfController.js
+++ src/controllers/CsrfController.js
@@ -2,7 +2,7 @@ import { generateCsrfToken } from '../middlewares/csrf.js'
 
 export class CsrfController {
   getCsrf = (req, res) => {
-    generateCsrfToken(req, res, { overwrite: true }) // Crea y envia la cookie con el CSRF Token
-    return res.json({ message: 'Token generado con éxito.' })
+    const csrfToken = generateCsrfToken(req, res, { overwrite: true }) // Crea y envia la cookie con el CSRF Token
+    return res.json({ message: 'Token generado con éxito.', data: csrfToken })
   }
 }
